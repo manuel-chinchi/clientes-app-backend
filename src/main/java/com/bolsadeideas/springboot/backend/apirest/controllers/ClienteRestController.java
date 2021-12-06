1c55bce703d1947e3f9d9f6e4226419ad9d77052
manuel-chinchi <manuel.chinchi93@gmail.com>
2021-12-06 07:45:25 -0300

    Update cliente component  - add CRUD methods in ClienteServiceImpl.java, ClienteRestController.java

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,12 +1,20 @@
 package com.bolsadeideas.springboot.backend.apirest.controllers;
 
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 import com.bolsadeideas.springboot.backend.apirest.models.services.IClienteService;
@@ -23,5 +31,33 @@ public class ClienteRestController {
 	public List<Cliente> index() {
 		return clienteService.findAll();
 	}
-
+	
+	@GetMapping("/clientes/{id}")
+	private Cliente show(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	@PostMapping("/clientes") // PostMapping -> es para crear
+	@ResponseStatus(code = HttpStatus.CREATED)
+	private Cliente create(@RequestBody Cliente cliente) { // RequestBody -> objeto json
+		cliente.setCreateAt(new Date());
+		return clienteService.save(cliente);
+	}
+	
+	@PutMapping("/clientes/{id}") // PutMapping -> es para actualizar
+	private Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente clienteActual = clienteService.findById(id);
+		
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setEmail(cliente.getEmail());
+		
+		return clienteService.save(clienteActual);
+	}
+	
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	private void delete(@PathVariable Long id) {
+		clienteService.delete(id);
+	}
 }
