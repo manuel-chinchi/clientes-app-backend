1c86dee0e0006e82c23da57fdd87fce0aec4cc6a
manuel-chinchi <manuel.chinchi93@gmail.com>
2021-12-26 23:22:31 -0300

    Remove unused libraries

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -8,12 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-//import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import com.sun.istack.Nullable;
 
 @Entity
 @Table(name="clientes")
